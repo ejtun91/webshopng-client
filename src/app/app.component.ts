8deddbd84e18b4e6eb72c4646c8c350152c38c08
Antonio K (ejtun91) <ejtun91@gmail.com>
2022-10-02 17:10:13 +0200

    'sec'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { UsersService } from './pages/login/services/users.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'webshop-client';
+  title = 'Webshop';
 
   constructor(private usersService: UsersService) {}
 
